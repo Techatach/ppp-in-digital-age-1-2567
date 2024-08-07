b2f3cf3df731198a36cf88f365e67947422709ff
Techatach <techatach@gmail.com>
2024-08-07 18:01:26 +0700

    first commit

---
+++ src/app/evaluation/page.tsx
@@ -0,0 +1,14 @@
+import Evaluation from '@/components/Evaluation'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Evaluation/>
+
+    </div>
+  )
+}
+
+
+
